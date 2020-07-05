fd87609b1814b4a6e88504558cff0a02532a65f1
Roberto Carboni <roberto.carboni@blackswan.com>
2020-07-05 18:03:41 +0100

    add build folder

---
+++ build/precache-manifest.077eb646bbe3fd8214e1cbb7e5f163da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da44bdded5f46de05632fa0cb0fcf8de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af7db28c72e5a88d7099",
+    "url": "/static/js/2.4b81a6d0.chunk.js"
+  },
+  {
+    "revision": "3bcb54e1d5251ef9190027b1a3d67523",
+    "url": "/static/js/2.4b81a6d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b76fc6fbd43bcbd5f8da",
+    "url": "/static/js/main.fea6633c.chunk.js"
+  },
+  {
+    "revision": "c7ca516702f23662fafe",
+    "url": "/static/js/runtime-main.5f12e899.js"
+  }
+]);
